f591ad120213c02dc74699558a07161f423ec0bf
abduzohir <88832552+abduzohir@users.noreply.github.com>
2022-11-19 11:59:34 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,4 @@
-function Summ(){return this.amount * this.price}
-function Kcall(){return this.amount * this.kcall}
-const products = {
-  plainBurger: {
-    name: 'Гамбургер простой',
-    price: 10000,
-    kcall: 500,
-    amount: 0,
-    Summ: Summ,
-    Kcall: Kcall,
-  },
-  freshBurger: {
-    name: 'Гамбургер FRESH',
-    price: 20500,
-    kcall: 700,
-    amount: 0,
-    Summ: Summ,
-    Kcall: Kcall,
-  },
-  freshCombo: {
-    name: 'FRESH COMBO',
-    price: 31900,
-    kcall: 1230,
-    amount: 0,
-    Summ: Summ,
-    Kcall: Kcall,
-  },
-}
-const extraProducts = {
-  doubleMayonnaise: {
-    name: 'Двойной майонез',
-    price: 500,
-    kcall: 200,
-  },
-  lettuce: {
-    name: 'Салатный лист',
-    price: 400,
-    kcall: 40,
-  },
-  cheese: {
-    name: 'Сыр',
-    price: 700,
-    kcall: 130,
-  },
-}
+
 const addCart = document.querySelector('.addCart')
 const receipt = document.querySelector('.receipt')
 const receiptWindow = document.querySelector('.receipt__window')
@@ -56,19 +12,6 @@ totalKcall = '@abduzohir_mmm',
 totalEmail = 'abduzohir7@gamil.com'
 
 addCart.addEventListener('click', function(){
-  for(const key in products){
-    const pObj = products[key]
-    if(pObj.amount > 0){
-      arrProducts.push(pObj)
-      pObj.name += `:${pObj.amount}`
-      for(const info in pObj){
-        if(pObj[info] === true){
-          pObj.name += `\n${extraProducts[info].name}`
-        }
-      }
-      pObj.name += `\nСтоимость: ${pObj.Summ()}\nКаллории: ${pObj.Kcall()}`
-    }
-  }
   for(let i = 0; i < arrProducts.length; i++){
     totalName += `\n${arrProducts[i].name}\n`
     totalPrice += arrProducts[i].Summ()
